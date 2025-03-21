d7d36ff58f9d984de9c58bd711edca3acb084693
Tushar Khatri <96564691+TUSHAR-KHATRI13@users.noreply.github.com>
2025-03-20 21:58:05 -0400

    Deleting tushars merged code in order to get rid of unseen conflicts

--- Backend/src/middlewares/authValidation.js
+++
@@ -1,9 +0,0 @@
-import { body } from 'express-validator';
-
-// Validation middleware for registration
-export const registerValidation = [
-  body('username').trim().isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
-  body('email').isEmail().withMessage('Please enter a valid email'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-  body('role').isIn(['user', 'venue_owner']).withMessage('Invalid role')
-]; 
