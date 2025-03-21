47896791b564291590881ce4475124620fab9a51
Tushar Khatri <96564691+TUSHAR-KHATRI13@users.noreply.github.com>
2025-03-21 07:11:47 +0530

    Revert "Login Functionality"

--- Backend/backend/src/index.js
+++ Backend/backend/src/index.js
@@ -1,22 +1,16 @@
-import express from "express";
-import cors from "cors";
+import expres from "express";
+import zod from "zod";
 import dotenv from "dotenv";
-import loginRoute from "./routes/login.js";  // Add .js at the end
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
-
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/login" , loginRoute);
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Something went wrong!' });
+app.use(express.json());
+
+app.get("/" , (req , res)=>{
+    res.send("Working ...");
 });
-  
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+app.listen(3000 , (req , res)=>{
+    console.log("Server is Running on Port 3000");
+}); 
